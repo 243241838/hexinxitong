6057240043efa5b2a30e309865fedd9d8b5fdf7e
gaoxiaofei <gaoxiaofei@lianqiangroup.com>
2018-03-01 11:41:09 +0800

    系统自己的分支

---
+++ src/utils/config.js
@@ -0,0 +1,32 @@
+const arr = [
+    // {customerDetail: 'customerList'},
+
+    //交易管理
+    {orderDetail: 'orderManage'},
+    {moneyDeal: 'dealStatistics'},
+    {policyDetails: 'policyManage'},
+    {dealManageCarFindListDetail: 'policyManage'},
+    {dealManagePersonInsuranceProDetail: 'policyManage'},
+    {dealManageCarFindListDetail: 'dealStatistics'},
+    {dealManagePersonInsuranceProDetail: 'dealStatistics'},
+    {dealManageCarFindListDetail: 'cancellationManage'},
+    {dealManagePersonInsuranceProDetail: 'cancellationManage'},
+    {dealManageCarFindListDetail: 'paymentRecord'},
+    {dealManagePersonInsuranceProDetail: 'paymentRecord'},
+    //系统管理
+    {operatorDetail: 'operatorManage'},
+    {permissionDetail:'operatorManage'},
+    {addOperatorDetail: 'operatorManage'},
+    {roleDetail:'roleManage'},
+    {resourceAccreditDetail: 'roleManage'},
+    {roleAddDetail: 'roleManage'},
+    {auditDetail: 'specialAuditManage'},
+    {productAuditDetail: 'productAuditManage'},
+    {systemMessagesEditDetail: 'systemMessagesManage'},
+    //客户端管理
+    {startManageAddDetail: 'startManage'},
+    {startManageEditDetail: 'startManage'},
+    {versionAddDetail: 'AndroidManage'},
+    {versionAddDetail: 'IOSManage'}
+]
+export default arr;
